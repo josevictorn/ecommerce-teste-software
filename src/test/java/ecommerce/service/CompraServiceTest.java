2fac71d6febbca1c2c2f2a4f6862da38f943b155
Vladimir Vieira <92822643+VladimirVieira@users.noreply.github.com>
2025-01-23 10:05:16 -0300

    Update CompraServiceTest.java

--- src/test/java/ecommerce/service/CompraServiceTest.java
+++ src/test/java/ecommerce/service/CompraServiceTest.java
@@ -353,5 +353,42 @@ public class CompraServiceTest {
 
     assertEquals(esperado, custoTotal);
   }
+
+  @Test
+  void calcularCustoTotal_clientePrata_comDescontoFrete_semDescontoItens_PesoLimite50kg() {
+     
+      Cliente cliente = new Cliente();
+      cliente.setTipo(TipoCliente.PRATA);
+
+     
+      Produto produto = new Produto();
+      produto.setPeso(50); // Peso unitário do produto
+      produto.setPreco(BigDecimal.valueOf(100));
+
+     
+      ItemCompra item = new ItemCompra();
+      item.setProduto(produto);
+      item.setQuantidade(1L); // Quantidade de itens
+
+    
+      List<ItemCompra> itens = Arrays.asList(item);
+
+      CarrinhoDeCompras carrinho = new CarrinhoDeCompras(
+        1L, 
+        cliente, 
+        itens, 
+        LocalDate.now()
+      );
+
+      // Cálculo do custo total com desconto de 50% no frete (cliente Prata)
+      BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho).setScale(1, RoundingMode.HALF_UP);
+      // Cálculo esperado: 100 * 1 = 100,00 (sem desconto nos itens)
+      // Peso total: 50 * 1 = 50kg
+      // Frete: 50kg * 4,00 (R$ 200,00) com desconto de 50%, ou seja, R$ 100,00
+      BigDecimal esperado = BigDecimal.valueOf(100 * 1).add(BigDecimal.valueOf(100)).setScale(1, RoundingMode.HALF_UP);
+      // Verificando se o custo total calculado é igual ao esperado
+      assertEquals(esperado, custoTotal);
+  }
+
 }
 //teste
